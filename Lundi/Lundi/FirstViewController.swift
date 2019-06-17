9afb0bbb4dcc6adc8cf4601305cd236d3ccbd7a0
matthewmu <matthewmu@kainos.com>
2019-06-17 17:28:20 +0100

    feature/inital-setup

---
+++ Lundi/Lundi/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Lundi
+//
+//  Created by Matthew Murray on 17/06/2019.
+//  Copyright © 2019 Matthew Murray. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Lundi/Lundi/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Lundi
+//
+//  Created by Matthew Murray on 17/06/2019.
+//  Copyright © 2019 Matthew Murray. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
